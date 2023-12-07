3c13ce2e0da5faaa6241bfcec8c2e5f162bce6d2
Behunin <l3ehunin@gmail.com>
2023-12-06 19:30:12 -0700

    Merge GithubReleases.ts into downloads.astro This moves the building of the releases list from the client to the server

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,31 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 const generateColorClass = (variable) => {
-	return ({ opacityValue }) =>
-		opacityValue
-			? `rgba(var(--${variable}), ${opacityValue})`
-			: `rgb(var(--${variable}))`
-}
+  return ({ opacityValue }) =>
+    opacityValue
+      ? `rgba(var(--${variable}), ${opacityValue})`
+      : `rgb(var(--${variable}))`;
+};
 
 const textColor = {
-	primary: generateColorClass('text-primary'),
-	secondary: generateColorClass('text-secondary'),
-	tertiary: generateColorClass('text-tertiary'),
-}
+  primary: generateColorClass("text-primary"),
+  secondary: generateColorClass("text-secondary"),
+  tertiary: generateColorClass("text-tertiary"),
+  link: generateColorClass("text-link"),
+};
 
 const backgroundColor = {
-	primary: generateColorClass('bg-primary'),
-	secondary: generateColorClass('bg-secondary'),
-	tertiary: generateColorClass('bg-tertiary'),
-}
+  primary: generateColorClass("bg-primary"),
+  secondary: generateColorClass("bg-secondary"),
+  tertiary: generateColorClass("bg-tertiary"),
+};
 
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
-	theme: {
-		extend: {
-			textColor,
-			backgroundColor,
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      textColor,
+      backgroundColor,
+    },
+  },
+  plugins: [],
+};
